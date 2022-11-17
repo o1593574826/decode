d1b622e44100806920b6adffd2aab9b5d1d8c6d0
o1593574826 <41886916+o1593574826@users.noreply.github.com>
2022-11-17 18:25:12 +0800

    Create pdecode.py

---
+++ pdecode.py
@@ -0,0 +1,10 @@
+import ddddocr
+
+def decode():
+    ocr = ddddocr.DdddOcr()
+    with open('captcha.png', 'rb') as f:
+        img_bytes = f.read()
+    res = ocr.classification(img_bytes)
+    # print(res)
+    return res
+    
